830829ec1c9531bbc929be7d972a1012fae2d7e8
kebryan <kebryan@DESKTOP-MT5UFPL>
2017-09-15 15:00:52 -0500

    git soft

--- mvc/controlador/C_MySQL.php
+++ mvc/controlador/C_MySQL.php
@@ -9,7 +9,7 @@ final class C_MySQL {
     }
 
     public function open() {
-        $this->mysqli = new mysqli("localhost", "root", "", "seg_graduados");
+        $this->mysqli = new mysqli("localhost", "root", "", "seg_graduados_mod");
         //$this->mysqli = new mysqli("localhost", "id2412119_root", "seg_graduados", "id2412119_seg_graduados");
         if ($this->mysqli->connect_errno) {
             echo "Fallo al conectar a MySQL: (" . $this->mysqli->connect_errno . ") " . $this->mysqli->connect_error;
